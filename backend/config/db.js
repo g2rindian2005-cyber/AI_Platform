e07c098c02c3b7d5b3211a6b7a711dc816305a0c
Gokul Rathod <g2rindian2005@gmail.com>
2026-09-15 13:17:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   max: 10,
   idleTimeoutMillis: 30000,
 });
